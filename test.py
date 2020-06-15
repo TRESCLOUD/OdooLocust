328890e578a9da9c4a866d99a8c88ee451b385c5
Patricio Rangles <patricio.rangles@trescloud.com>
2020-06-15 18:19:25 -0500

    Parametros deprecados

--- test.py
+++ test.py
@@ -5,8 +5,12 @@ from OdooLocust import OdooTaskSet
 class Seller(OdooLocust.OdooLocust):
     host = "127.0.0.1"
     database = "testdb"
-    min_wait = 100
-    max_wait = 1000
+    #Deprecado en las ultimas version de locust
+    #min_wait = 100
+    #max_wait = 1000
+    #
+    #ahora se utiliza el siguente parametro
+    wait_time = 0.5
     weight = 3
     
     task_set = OdooTaskSet.OdooGenericTaskSet
